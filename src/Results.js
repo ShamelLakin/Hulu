6bac493390e987395728a1933c71854304dd6a46
Shamel Lakin <christlife4u@gmail.com>
2021-03-05 12:52:48 -0500

    API keys being implemented

--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,24 @@
 import React from "react";
-import './styles/Results.css'
-import VideoCard from './VideoCard'
+import "./styles/Results.css";
+import VideoCard from "./VideoCard";
 
 function Results() {
-  return <div className="results">
-     <VideoCard /> 
-  </div>;
+  return (
+    <div className="results">
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+    </div>
+  );
 }
 
 export default Results;
--- src/VideoCard.js
+++ src/VideoCard.js
@@ -4,9 +4,9 @@ import './styles/VideoCard.css'
 function VideoCard({ movie }) {
     return (
       <div className="videoCard">
-        <h1>Hello</h1>
+        {/* <h1>Hello</h1> */}
         <img
-          src="https://media.istockphoto.com/vectors/movie-time-vector-illustration-cinema-poster-concept-on-red-round-vector-id911590226?k=6&m=911590226&s=612x612&w=0&h=u6vP2FnJG8Ib3O1xofOUeJ5NtHWrWdRnV-OSL8arBnk="
+          src="https://techcrunch.com/wp-content/uploads/2016/11/hulu-shows-movies.jpg"
           alt=""
         />
         <p>Overview</p>
